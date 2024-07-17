cded2b51adefe87af62c50ed80227c445d78270a
Shivathmika20 <yelurishivathmika@gmail.com>
2024-07-17 13:34:33 +0530

    animation: Hero

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='text-center bg-[linear-gradient(to_right,rgb(252,214,255,0.7),rgb(41,216,255,0.7),rgb(255,253,128,0.7),rgb(248,154,191,0.7),rgb(252,214,255,0.7))]'>
+    <div className='text-center bg-[linear-gradient(to_right,rgb(252,214,255,0.7),rgb(41,216,255,0.7),rgb(255,253,128,0.7),rgb(248,154,191,0.7),rgb(252,214,255,0.7))] '>
         <div className="container p-4 flex justify-center items-center mx-auto">
             <p><span className='hidden text-center font-medium sm:inline'>This page is included in a free SaaS Website Kit- </span>
             <a href="#" className='underline underline-offset-4 font-medium'>Explore the demo</a>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,10 @@
+'use client'
 import React from 'react'
 import arrow from '../public/arrow.png'
 import Image from 'next/image'
 import cursor from '../public/cursor 1.png'
 import msg from '../public/message 1.png'
-
+import {motion } from 'framer-motion'
 function Hero() {
   return (
     <div className='bg-black text-white bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] py-16 sm:py-22 relative overflow-clip'>
@@ -23,10 +24,16 @@ function Hero() {
                   One Task
                   <br />at a Time
               </h1>
-              
-                <Image src={cursor} width={200} height={200} alt='cursor' className='absolute right-[476px] top-[100px] hidden sm:inline'/>
-                <Image src={msg} width={200} height={200} alt='msg' className='absolute left-[498px] top-14 hidden sm:inline'/>
-              
+              <motion.div className='absolute right-[480px] top-[80px] hidden sm:inline '
+                drag
+                dragSnapToOrigin>
+                  <Image src={cursor} width={200} height={200} alt='cursor' className='max-w-none' draggable={false}/>
+              </motion.div>
+              <motion.div className='absolute left-[498px] top-14 hidden sm:inline'
+                drag
+                dragSnapToOrigin> 
+                  <Image src={msg} width={200} height={200} alt='msg' className='max-w-none' draggable={false}/>
+              </motion.div>
               </div>
             </div>
             <p className='text-center mt-8 text-xl max-w-md mx-auto'>
