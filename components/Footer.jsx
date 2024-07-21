fbef65f3b7ba3924ca787850be9122a201c8a0d1
Shivathmika20 <yelurishivathmika@gmail.com>
2024-07-21 20:24:39 +0530

    style: Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,15 +9,15 @@ import pin from '../public/pinterest.png'
 function Footer() {
   return (
     <div className='bg-black text-white py-8'>
-        <div className='px-6 flex flex-col justify-between items-center sm:flex-row border-t border-white/30'>
-            <p className='mt-5 text-white/50'>@ 2024 Your Company, Inc. All rights reserved</p>
+        <div className='max-w-screen-xl mx-auto px-6 flex flex-col justify-between items-center sm:flex-row border-t border-white/30'>
+            <p className='mt-5 text-white/50 text-center'>@ 50024 Your Company, Inc. All rights reserved</p>
             <div className='flex gap-4 mt-4 p-3'>
-                <Image src={insta} alt='insta' width={20} height={20} className='h-6'/>
-                <Image src={twitter} alt='twitter' width={20} height={20} className='h-6'/>
-                <Image src={link} alt='link' width={20} height={20} className='h-6'/>
-                <Image src={music} alt='music' width={20} height={20}  className='h-6'/>
-                <Image src={utube} alt='utube' width={20} height={20}  className='h-6'/>
-                <Image src={pin} alt='pin' width={20} height={20}  className='h-6'/>
+                <Image src={insta} alt='insta' width={500} height={500} className='size-4 md:size-6'/>
+                <Image src={twitter} alt='twitter' width={500} height={500} className='size-4 md:size-6'/>
+                <Image src={link} alt='link' width={500} height={500} className='size-4 md:size-6'/>
+                <Image src={music} alt='music' width={500} height={500}  className='size-4 md:size-6'/>
+                <Image src={utube} alt='utube' width={500} height={500}  className='size-4 md:size-6'/>
+                <Image src={pin} alt='pin' width={500} height={500}  className='size-4 md:size-6'/>
             </div>
         </div>
 
